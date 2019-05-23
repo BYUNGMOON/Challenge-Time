906cf7feae99f35e0563221de7424cf4af23c024
Byungmoon Kim <byungmoon.k@sea.samsung.com>
2019-05-23 16:56:46 -0500

    Modified Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -3,5 +3,8 @@ class Mobile {
     public static void main( String[] args) {
         system.out.println("test");
         system.out.println("Hi World");
+
+        system.out.println("Hi Everyone");
+
     }
 }
